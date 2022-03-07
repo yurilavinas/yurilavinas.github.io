1ccf0479c8a45444955318f469df236451c5a71f
yurilavinas <yclavinas@gmail.com>
2022-03-07 13:27:03 +0900

    new blog post

--- exec.R
+++ exec.R
@@ -5,9 +5,12 @@ library(beepr)
 library(dplyr)
 
 # setwd("~/Documents/estudos/webpage/")
+rmarkdown::render('blog.Rmd', 'html_document')
 
 knitr::opts_chunk$set(echo = TRUE, include = TRUE, cache=T, fig.width = 8, fig.height=6, fig.align="center", background = c(.95,.95,.95),message=FALSE)
 
 #Execute
 render_site()
+
+
 beep()  #finished rendering
