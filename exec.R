4a1470188bac373b65747d68286cc3f8cf0ce922
yurilavinas <yclavinas@gmail.com>
2023-06-27 12:13:56 +0200

    new papers

--- exec.R
+++ exec.R
@@ -10,6 +10,7 @@ rmarkdown::render('publications.Rmd', 'html_document')
 knitr::opts_chunk$set(echo = TRUE, include = TRUE, cache=T, fig.width = 8, fig.height=6, fig.align="center", background = c(.95,.95,.95),message=FALSE)
 
 #Execute
+render_site(input = "index.Rmd")
 render_site(input = "publications.Rmd")
 
 
