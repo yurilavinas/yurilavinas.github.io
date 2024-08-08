7e5b02535d8a5bc5a00129fb22737e36348b1a00
yurilavinas <yclavinas@gmail.com>
2024-08-08 16:22:34 +0200

    size

--- exec.R
+++ exec.R
@@ -7,11 +7,11 @@ library(dplyr)
 # setwd("~/Documents/estudos/webpage/")
 rmarkdown::render('publications.Rmd', 'html_document')
 
-knitr::opts_chunk$set(echo = TRUE, include = TRUE, cache=T, fig.width = 8, fig.height=6, fig.align="center", background = c(.95,.95,.95),message=FALSE)
-
-#Execute
-render_site(input = "index.Rmd")
-render_site(input = "publications.Rmd")
+# knitr::opts_chunk$set(echo = TRUE, include = TRUE, cache=T, fig.width = 8, fig.height=6, fig.align="center", background = c(.95,.95,.95),message=FALSE)
+# 
+# #Execute
+# render_site(input = "index.Rmd")
+# render_site(input = "publications.Rmd")
 
 
 
