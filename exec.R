e78a98dfc666381b4c3a4d57eef4d3fab7e895c8
yurilavinas <yclavinas@gmail.com>
2024-10-16 11:05:24 +0200

    preprints

--- exec.R
+++ exec.R
@@ -4,14 +4,14 @@ library(knitr)
 # library(render_site)
 library(dplyr)
 
-# setwd("~/Documents/estudos/webpage/")
+setwd("~/Documents/yurilavinas.github.io/")
 rmarkdown::render('publications.Rmd', 'html_document')
 
 # knitr::opts_chunk$set(echo = TRUE, include = TRUE, cache=T, fig.width = 8, fig.height=6, fig.align="center", background = c(.95,.95,.95),message=FALSE)
 # 
 # #Execute
 # render_site(input = "index.Rmd")
-# render_site(input = "publications.Rmd")
+render_site(input = "publications.Rmd")
 
 
 
