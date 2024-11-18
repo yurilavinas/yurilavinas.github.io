77e471257e4074928353c40ea72d7b0f92b64e42
yurilavinas <yclavinas@gmail.com>
2024-11-18 12:39:03 +0100

    calendae=r

--- exec.R
+++ exec.R
@@ -1,7 +1,6 @@
 #Setup
 library(rmarkdown)
 library(knitr)
-library(render_site)
 library(dplyr)
 
 setwd("~/Documents/yurilavinas.github.io/")
@@ -10,8 +9,9 @@ setwd("~/Documents/yurilavinas.github.io/")
 # knitr::opts_chunk$set(echo = TRUE, include = TRUE, cache=T, fig.width = 8, fig.height=6, fig.align="center", background = c(.95,.95,.95),message=FALSE)
 # 
 # #Execute
-render_site(input = "calendar.Rmd")
-# render_site(input = "publications.Rmd")
+render_site(input = "index.Rmd")
+render_site(input = "publications.Rmd")
+render_site(input = "blog.Rmd")
 
 
 
