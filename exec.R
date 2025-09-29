b0011f203cfee29eefc9c2aae3318ef1db5952f5
Yuri Lavinas <yclavinas@gmail.com>
2025-09-29 10:02:29 -0400

    2025 pubs

--- exec.R
+++ exec.R
@@ -3,11 +3,11 @@ library(rmarkdown)
 library(knitr)
 library(dplyr)
 
-setwd("~/Documents/yurilavinas.github.io/")
+setwd("~/Documents/MCF/yurilavinas.github.io/")
 
 # #Execute
-# render_site(input = "index.Rmd")
-render_site(input = "publications.Rmd")
+render_site(input = "index.Rmd")
+# render_site(input = "publications.Rmd")
 # render_site(input = "calendar.Rmd")
 # render_site(input = "blog.Rmd")
 # render_site(input = "my_blog/visuals.Rmd")
