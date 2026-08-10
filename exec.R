0423ad2cb956d847906e87dcd10b6ea6134a1596
Yuri Lavinas <yclavinas@gmail.com>
2026-08-10 19:26:27 -0300

    new position

--- exec.R
+++ exec.R
@@ -6,11 +6,11 @@ library(dplyr)
 setwd("~/Documents/MCF/yurilavinas.github.io/")
 
 # #Execute
-# render_site(input = "index.Rmd")
-# render_site(input = "aulas.Rmd")
+render_site(input = "index.Rmd")
+render_site(input = "aulas.Rmd")
 render_site(input = "publications.Rmd")
-# render_site(input = "blog.Rmd")
-# render_site(input = "my_blog/visuals.Rmd")
+render_site(input = "blog.Rmd")
+render_site(input = "my_blog/visuals.Rmd")
 
 
 
