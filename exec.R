e421defd298e3610d19bba72a5e3d5dca932234e
Yuri Lavinas <yclavinas@gmail.com>
2026-08-24 09:37:10 -0300

    aulas

--- exec.R
+++ exec.R
@@ -10,7 +10,7 @@ render_site(input = "index.Rmd")
 render_site(input = "aulas.Rmd")
 render_site(input = "publications.Rmd")
 render_site(input = "blog.Rmd")
-render_site(input = "my_blog/visuals.Rmd")
+# render_site(input = "my_blog/visuals.Rmd")
 
 
 
